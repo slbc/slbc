7295ef44990dfeecc6a685dec883bddeed61cb50
= <mpretty@voceconnect.com>
2013-09-25 19:22:32 -0400

    optimizing theme images

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    imagemin: {
+      theme: {
+        files: [
+          {
+            expand: true,
+            cwd: "wp-content/themes/designfolio-pro/",
+            src: "**/*.{png,jpg}",
+            dest: "wp-content/themes/designfolio-pro/"
+          }
+        ]
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['imagemin']);
+
+};
